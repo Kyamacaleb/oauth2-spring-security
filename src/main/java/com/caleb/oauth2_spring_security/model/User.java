96ba3b75a1f18b9de67eb306e05ab339892b61d2
Caleb kyama <91477041+Kyamacaleb@users.noreply.github.com>
2024-09-17 12:44:51 +0300

    Initial commit

---
+++ src/main/java/com/caleb/oauth2_spring_security/model/User.java
@@ -0,0 +1,26 @@
+package com.caleb.oauth2_spring_security.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String email;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
